2e667d10e36784b3894a94afba9f2a61086e824d
davelush <dave.lush@gmail.com>
2023-04-22 07:50:17 +0100

    Added a first application command and some basic instructions for getting started

--- README.md
+++ README.md
@@ -2,5 +2,17 @@
 A bot to support induction and tooling requests for rLab
 
 
-https://discord.com/developers/applications
-https://discordpy.readthedocs.io/en/stable/discord.html
+## Developing locally
+
+- [ ] Follow step 1 of the [Discord developer docs - getting started](https://discord.com/developers/docs/getting-started)
+to create and install an app with permissions
+- [ ] In the [Discord application](https://discord.com/developers/applications) go to `Bot` and turn on the different 
+`Privileged Gateway Intents`. These are needed for the Python library this bot uses to integrate with Discord. 
+- [ ] Install [ngrok](https://ngrok.com/download) so that you can create a local tunnel 
+- [ ] Run `ngrok http 8080` in the console
+- [ ] On your [Discord application](https://discord.com/developers/applications) set the `General information -> 
+interactions endpoint url` to `[ngrok forwarding url]/api/commands`
+
+## Random spare links
+
+- https://discordpy.readthedocs.io/en/stable/discord.html
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+import os
 import discord
+from discord import app_commands
+
+bot_token = os.getenv('BOT_TOKEN')
+guild_id = os.getenv('GUILD_ID')
 
 intents = discord.Intents.default()
-intents.message_content = True
-
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+# TODO This guild id can be removed in production but the commands will take about an hour to register. Probably worth
+# doing this as an environment variable so that it is None if not set
+@tree.command(name="test_command", description="The best bot command ever!", guild=discord.Object(id=guild_id))
+async def test_command(interaction):
+    await interaction.response.send_message("Hello!")
+    print("Ready!")
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    await tree.sync(guild=discord.Object(id=guild_id))
+    print("Ready!")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    print(message)
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run('put your token here')
+client.run(bot_token)
